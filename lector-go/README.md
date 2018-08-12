de125705a4db502a34fa939059e68caf3948741a
Diego Abanto <dabanto@belatrixsf.com>
2018-08-12 09:29:53 -0500

    Escribir en ficheros

--- lector-go/README.md
+++ lector-go/README.md
@@ -9,3 +9,22 @@ Para leer ficheros debemos hacer uso de la libreria **io/ioutil**
     
     $ texto, error := ioutil.ReadFile("fichero.txt")
 
+## Escribir ficheros
+
+Para escribir texto en el archivo se debe tener en cuenta lo siguiente
+
+1. Leer valores ingresados
+
+        $ nuevoTexto := os.Args[1] + "\n"
+
+2. Abrir el fichero
+
+        $ fichero, err := os.OpenFile("fichero.txt", os.O_APPEND|os.O_WRONLY, 0777)
+
+3. Escribir en el fichero
+
+        $ escribir, err := fichero.WriteString(nuevoTexto)
+
+4. Cerrar el fichero
+
+        $ fichero.Close()
--- lector-go/lector.go
+++ lector-go/lector.go
@@ -3,14 +3,34 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 )
 
 func main() {
 
 	fmt.Println("Lector")
 
-	texto, error := ioutil.ReadFile("fichero.txt")
-	showError(error)
+	// Write file
+	nuevoTexto := os.Args[1] + "\n"
+
+	// Overwrite file
+	//nuevoTexto := []byte(os.Args[1])
+	//escribir := ioutil.WriteFile("fichero.txt", nuevoTexto, 0777)
+	//showError(escribir)
+
+	// Append text to the file
+	fichero, err := os.OpenFile("fichero.txt", os.O_APPEND|os.O_WRONLY, 0777)
+	showError(err)
+
+	escribir, err := fichero.WriteString(nuevoTexto)
+	fmt.Println(escribir)
+	showError(err)
+
+	fichero.Close()
+
+	// Read file
+	texto, errorDeFichero := ioutil.ReadFile("fichero.txt")
+	showError(errorDeFichero)
 
 	fmt.Println(string(texto))
 }
